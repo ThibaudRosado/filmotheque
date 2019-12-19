48b4c4fe63cd2ef5d83b643019c76906a5442bbc
Rosado Thibaud <58431334+ThibaudRosado@users.noreply.github.com>
2019-12-19 10:45:44 +0100

    Code clean ++

--- code.js
+++ code.js
@@ -3,7 +3,6 @@ var idUnique = 0;
 var idSearch = 0;
 var trieAnnee = 0;
 var trieTitre = 0;
-var variableAddFilm = 0;
 
 
 /**Permet de supprimer un film et son affichage */
@@ -42,79 +41,17 @@ function showAllCard() {
         }
     }
 }
-/** Sauvegarde un film dans l'array*/
-function saveFilm(film) {
-    idUnique++;
-    arrayTab.push({ id: idUnique, titre: film.title, annee: parseInt(film.release_date.slice(0, 4), 10), realisateur: "", affiche: "https://image.tmdb.org/t/p/w600_and_h900_bestv2" + film.poster_path, description: film.overview });
-    creerCardFirstPos(arrayTab[arrayTab.length - 1]);
-}
-/**Creer un formulaire d'ajout d'un film */
-function createFormAddFilm() {
-    if (variableAddFilm == 0) {
-        var main = document.getElementById("add");
-
-        var divInput = document.createElement("div");
-        divInput.className = "ui input";
-        divInput.id = "form";
-
-        var input = document.createElement("input");
-        input.type = "text";
-        input.id = "titreAdd";
-        input.placeholder = "Entrer titre";
-        input.className = "ui input";
-
-        var btnAdd = document.createElement("button");
-        btnAdd.className = "ui blue big button";
-        btnAdd.innerText = "Rechercher";
-        btnAdd.addEventListener('click', function () {
-            if (input.value.length > 1) {
-                dropAllCard();
-                btnFilmSearch(input.value);
-            } else {
-                alert("Un titre doit au moins avoir 2 lettres");
-            }
-        });
-
-        var btnAnnuler = document.createElement("button");
-        btnAnnuler.className = "ui grey big button";
-        btnAnnuler.innerText = "Annuler";
-        btnAnnuler.addEventListener('click', function () {
-            dropFormAddFilm();
-            dropAllCard();
-            showAllCard();
-            creerBtnTrie();
-        });
-        main.appendChild(divInput);
-        divInput.appendChild(input);
-        divInput.appendChild(btnAdd);
-        divInput.appendChild(btnAnnuler);
-
-        variableAddFilm = 1;
+/** Permet d'afficher tout les films en mettant le dernier éléments qui a été ajouté en évidance */
+function showAllCardWithNewFirst() {
+    for (var i = 0; i < idUnique - 1; i++) {
+        if (arrayTab[i]) {
+            creerCard(arrayTab[i]);
+        }
     }
+    creerCardFirstPos(arrayTab[idUnique - 1])
 }
-/**Supprime le formulaire d'ajout */
-function dropFormAddFilm() {
-    document.getElementById("form").remove();
-    variableAddFilm = 0;
-}
-
-
-
-
-function rechercherFilm() {
-    createFormAddFilm();
-    dropAllCard();
-    supprimerFleche();
-
-    let titre = document.getElementById("titre");
-    let annee = document.getElementById("annee");
-    titre.remove();
-    annee.remove();
-}
-function btnFilmSearch(titre) {
-    callAPIsearch(titre, listSearch);
-}
-function listSearch(rep) {
+/** Permet d'afficher tout les résultats de la recherche */
+function showAllCardSearch(rep) {
     let i = 0;
     if (rep.results.length != 0) {
         while (i < rep.results.length) {
@@ -125,6 +62,93 @@ function listSearch(rep) {
             i++;
         }
     } else { alert("Pas de résultat pour cette recherche.") }
+}
+/**Creer un formulaire d'ajout d'un film */
+function createFormAddFilm() {
+    //On créé tout les composants du formulaire de recherche
+    var main = document.getElementById("add");
+
+    var divInput = document.createElement("div");
+    divInput.className = "ui input";
+    divInput.id = "form";
+
+    var input = document.createElement("input");
+    input.type = "text";
+    input.id = "titreAdd";
+    input.placeholder = "Entrer titre";
+    input.className = "ui input";
+
+    var btnAdd = document.createElement("button");
+    btnAdd.className = "ui blue big button";
+    btnAdd.innerText = "Rechercher";
+    btnAdd.addEventListener('click', function () {
+        if (input.value.length > 1) {
+            dropAllCard();
+            callAPIsearch(input.value, showAllCardSearch);
+        } else {
+            alert("Un titre doit au moins avoir 2 lettres");
+        }
+    });
+
+    var btnAnnuler = document.createElement("button");
+    btnAnnuler.className = "ui grey big button";
+    btnAnnuler.innerText = "Annuler";
+    btnAnnuler.addEventListener('click', function () {
+        dropFormAddMovie();
+        dropAllCard();
+        showAllCard();
+        resetMenu();
+    });
+    //On les relis a notre page html
+    main.appendChild(divInput);
+    divInput.appendChild(input);
+    divInput.appendChild(btnAdd);
+    divInput.appendChild(btnAnnuler);
+
+
+}
+/**Supprime le formulaire d'ajout */
+function dropFormAddMovie() {
+    document.getElementById("form").remove();
+    variableAddFilm = 0;
+}
+/** Permet d'initialiser la page de recherche de film */
+function menuSearchMovie() {
+    //On supprimer la possiblité d'ouvrire a nouveau un formulaire
+    document.getElementById("search").onclick = null;
+    //On crée le formulaire de recherche
+    createFormAddFilm();
+    //On enlève toute les films qui pourait être la 
+    dropAllCard();
+    //On supprime la flèche qui indique les sens de tries
+    supprimerFleche();
+    // On supprimer les onglets de trie
+    let titre = document.getElementById("titre");
+    let annee = document.getElementById("annee");
+    titre.remove();
+    annee.remove();
+}
+/** Permet de réinitialiser le menu */
+function resetMenu() {
+    let titre = document.createElement("a");
+    titre.id = "titre";
+    titre.className = "item";
+    titre.onclick = trieParTitre;
+    titre.innerHTML = "Titre";
+
+    let annee = document.createElement("a");
+    annee.id = "annee";
+    annee.className = "item";
+    annee.onclick = trieParAnnee;
+    annee.innerHTML = "Année";
+
+    let nav = document.getElementById("nav");
+    var collNoeuds = nav.childNodes;
+    nav.insertBefore(annee, collNoeuds[0]);
+    nav.insertBefore(titre, annee);
+
+    let search = document.getElementById("search");
+    search.onclick = menuSearchMovie;
 }
 /** Permet de récupérer les informations d'un film avec le site www.themoviedb.org */
 function callAPIsearch(name, callback2) {
@@ -142,36 +166,13 @@ function callAPIsearch(name, callback2) {
     });
 
 }
-function afficheAllNew() {
-    for (var i = 0; i < idUnique - 1; i++) {
-        if (arrayTab[i]) {
-            creerCard(arrayTab[i]);
-        }
-    }
-    creerCardFirstPos(arrayTab[idUnique - 1])
+/** Sauvegarde un film dans l'array */
+function saveMovie(film) {
+    idUnique++;
+    arrayTab.push({ id: idUnique, titre: film.title, annee: parseInt(film.release_date.slice(0, 4), 10), realisateur: "", affiche: "https://image.tmdb.org/t/p/w600_and_h900_bestv2" + film.poster_path, description: film.overview });
+    creerCardFirstPos(arrayTab[arrayTab.length - 1]);
 }
-function creerBtnTrie() {
-    let titre = document.createElement("a");
-    titre.id = "titre"
-    titre.className = "item"
-    titre.onclick = trieParTitre
-    titre.innerHTML = "Titre"
-
-    let annee = document.createElement("a")
-    annee.id = "annee"
-    annee.className = "item"
-    annee.onclick = trieParAnnee
-    annee.innerHTML = "Année"
-
-    let nav = document.getElementById("nav")
-    var collNoeuds = nav.childNodes;
-    nav.insertBefore(annee, collNoeuds[0]);
-    nav.insertBefore(titre, annee);
-}
-
-
-
-/** Permet de créer une bouvelle card qui s'affiche en première */
+/** Permet de créer une card qui s'affiche en première */
 function creerCardFirstPos(monObjet) {
     var divCards = document.getElementById("ListCard");
     // Création des différants elléments
@@ -211,6 +212,7 @@ function creerCardFirstPos(monObjet) {
     divCard.appendChild(divExtraContent);
     divExtraContent.appendChild(spanFloated);
 }
+/** Permet de créer une card qui s'enregistre quand on click dessus */
 function creerCardForSearch(monObjet, film) {
     var divCards = document.getElementById("ListCard");
     // Création des différants elléments
@@ -220,12 +222,12 @@ function creerCardForSearch(monObjet, film) {
     divCard.addEventListener('click', function () {
         var bool = confirm("Voulez vous ajouter ce film a votre filmotèque ?")
         if (bool) {
-            saveFilm(film);
+            saveMovie(film);
             dropAllCard();
-            afficheAllNew();
-            dropFormAddFilm();
+            showAllCardWithNewFirst();
+            dropFormAddMovie();
             window.scroll(0, 0);
-            creerBtnTrie();
+            resetMenu();
             alert("Film ajoutée avec succé!");
         }
     });
@@ -260,7 +262,7 @@ function creerCardForSearch(monObjet, film) {
     divCard.appendChild(divExtraContent);
     divExtraContent.appendChild(spanFloated);
 }
-/** Permet de créer une card en fonction d'un objet */
+/** Permet de créer une card qui se supprime quand on click dessus */
 function creerCard(monObjet) {
     var divCards = document.getElementById("ListCard");
     // Création des différants elléments
@@ -279,8 +281,6 @@ function creerCard(monObjet) {
     divHeader.innerText = monObjet.titre;
     var divMeta = document.createElement("div");
     divMeta.className = "meta";
-    //divMeta.innerText = "Real : " + monObjet.realisateur + " - Sortie en " + monObjet.annee;
-    //divMeta.innerText = "Real : " + monObjet.realisateur;
     var divDescription = document.createElement("div");
     divDescription.className = "description";
     divDescription.innerText = monObjet.description.slice(0, 150) + " ...";
@@ -302,26 +302,25 @@ function creerCard(monObjet) {
     divCard.appendChild(divExtraContent);
     divExtraContent.appendChild(spanFloated);
 }
-
-
-
 /**Trie par titre */
 function trieParTitre() {
     dropAllCard();
     arrayTab.sort(compareTitre);
     if (trieTitre == 0) {
         supprimerFleche();
-    let icon = document.createElement("i");
-    icon.className = "caret down icon";
-    icon.id = "caret";
-    document.getElementById("titre").insertAdjacentElement('beforeend', icon);
+        // créé une flèche pour indiquer le sens du trie
+        let icon = document.createElement("i");
+        icon.className = "caret down icon";
+        icon.id = "caret";
+        document.getElementById("titre").insertAdjacentElement('beforeend', icon);
         trieTitre = 1;
     } else {
         supprimerFleche();
-    let icon = document.createElement("i");
-    icon.className = "caret up icon";
-    icon.id = "caret";
-    document.getElementById("titre").insertAdjacentElement('beforeend', icon);
+        // créé une flèche pour indiquer le sens du trie
+        let icon = document.createElement("i");
+        icon.className = "caret up icon";
+        icon.id = "caret";
+        document.getElementById("titre").insertAdjacentElement('beforeend', icon);
         trieTitre = 0;
     }
     showAllCard();
@@ -332,6 +331,7 @@ function trieParAnnee() {
     arrayTab.sort(compareAnnee);
     if (trieAnnee == 0) {
         supprimerFleche();
+        // créé une flèche pour indiquer le sens du trie
         let icon = document.createElement("i");
         icon.className = "caret down icon";
         icon.id = "caret";
@@ -339,6 +339,7 @@ function trieParAnnee() {
         trieAnnee = 1;
     } else {
         supprimerFleche();
+        // créé une flèche pour indiquer le sens du trie
         let icon = document.createElement("i");
         icon.className = "caret up icon";
         icon.id = "caret";
@@ -398,38 +399,32 @@ function supprimerFleche() {
     }
 }
 
-
-
-
+//Portion d'initialisation de la page
 addPleinDeFilm();
-/** Ajoute un jeu de film test suplémentaire*/
+/** Ajoute un jeu de film test*/
 function addPleinDeFilm() {
-    addFilm('Jumanji : next level');
-    addFilm('Rambo: Last Blood');
-    addFilm('The Irishman');
-    addFilm('Joker');
-    addFilm('Venom');
-    addFilm('Wonder Woman');
-    addFilm('Avengers : L\'Ère d\'Ultron');
-    addFilm('James Bond');
-    addFilm('Aquaman');
-    addFilm('Fury');
-    addFilm('Casino Royale');
-    addFilm('Iron Man');
-    addFilm("La Reine des neiges 2");
-    addFilm('Code 8');
-    addFilm("Boulevard du crépuscule");
-    addFilm("Notre-Dame de Paris");
-    addFilm("Le Loup de Wall Street");
-    addFilm("Midnight Express");
-    addFilm("Harry Potter");
-    addFilm("teddy");
+    callAPI('Jumanji : next level', saveMovie);
+    callAPI('Rambo: Last Blood', saveMovie);
+    callAPI('The Irishman', saveMovie);
+    callAPI('Joker', saveMovie);
+    callAPI('Venom', saveMovie);
+    callAPI('Wonder Woman', saveMovie);
+    callAPI('Avengers : L\'Ère d\'Ultron', saveMovie);
+    callAPI('James Bond', saveMovie);
+    callAPI('Aquaman', saveMovie);
+    callAPI('Fury', saveMovie);
+    callAPI('Casino Royale', saveMovie);
+    callAPI('Iron Man', saveMovie);
+    callAPI("La Reine des neiges 2", saveMovie);
+    callAPI('Code 8', saveMovie);
+    callAPI("Boulevard du crépuscule", saveMovie);
+    callAPI("Notre-Dame de Paris", saveMovie);
+    callAPI("Le Loup de Wall Street", saveMovie);
+    callAPI("Midnight Express", saveMovie);
+    callAPI("Harry Potter", saveMovie);
+    callAPI("teddy", saveMovie);
 }
-/** Ajoute un film */
-function addFilm(titre) {
-    callAPI(titre, saveFilm);
-}
-/** Permet de récupérer les informations d'un film avec le site www.themoviedb.org */
+/** Permet de récupérer les informations d'un film avec le site www.themoviedb.org pour le jeu de test */
 function callAPI(name, callback) {
     var settings = {
         "async": true,
